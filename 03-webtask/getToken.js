89338f580ab2f33f5904fbad2d6f6365d1891a42
Ricardo Batista <ricardo.batista@auth0.com>
2017-06-19 09:27:35 +0100

    Updated Webtask sample

--- 03-webtask/getToken.js
+++ 03-webtask/getToken.js
@@ -1,14 +1,19 @@
 module.exports = function (cb) { 
 	var request = require("request");
 
+	var AUTH0_DOMAIN = "";
+    var AUTH0_CLIENT_ID = "";
+    var AUTH0_CLIENT_SECRET = "";
+    var AUTH0_AUDIENCE_API = "https://" + AUTH0_DOMAIN + "/api/v2/";
+
 	var options = { method: 'POST',
-	  url: 'https://{AUTH0_DOMAIN}/oauth/token',
+	  url: 'https://' + AUTH0_DOMAIN + '/oauth/token',
 	  headers: { 'content-type': 'application/json' },
 	  body: 
 	   { grant_type: 'client_credentials',
-	     client_id: '{AUTH0_CLIENT_ID}',
-	     client_secret: '{AUTH0_CLIENT_SECRET}',
-	     audience: 'https://{AUTH0_DOMAIN}/api/v2/' },
+	     client_id: AUTH0_CLIENT_ID,
+	     client_secret: AUTH0_CLIENT_SECRET,
+	     audience: AUTH0_AUDIENCE_API },
 	  json: true };
 
 	request(options, function (error, response, body) {
